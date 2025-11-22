ca4ea244da24e3eb0968fd504e3245ecfee0ce39
Dima <dima@test.test>
2025-11-22 09:24:11 +0300

    Initial commit: Added basic motor control script

---
+++ motors_control.py
@@ -0,0 +1,14 @@
+#!/bin/env python3
+
+# motors_control.py
+def turn_on_motors():
+    # Включить моторы
+    print("Моторы ВКЛ.")
+
+def turn_off_motors():
+    # Выключить моторы
+    print("Моторы ВЫКЛ.")
+
+# Главный цикл
+turn_on_motors()
+turn_off_motors()
